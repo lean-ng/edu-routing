e90900bcc408d7d75b552cdab03f1eed72432879
Micha Alt <ng@lean-stack.de>
2019-02-05 21:58:15 +0100

    Demoing a redirect route. No need for routerLinkActiveOptions now.

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -5,7 +5,8 @@ import { LoginComponent } from './pages/login/login.component';
 import { ErrorNotFoundComponent } from './pages/error-not-found/error-not-found.component';
 
 export const appRoutes: Route[] = [
-  { path: '', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'login', component: LoginComponent },
   { path: '**', component: ErrorNotFoundComponent },
