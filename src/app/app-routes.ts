a8936618e7774bd6342fe6e5675e56ab2a491f8b
Micha Alt <ng@lean-stack.de>
2019-02-05 21:55:52 +0100

    Adding a wildcard route.

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -2,9 +2,11 @@ import { Route } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { LoginComponent } from './pages/login/login.component';
+import { ErrorNotFoundComponent } from './pages/error-not-found/error-not-found.component';
 
 export const appRoutes: Route[] = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'login', component: LoginComponent },
+  { path: '**', component: ErrorNotFoundComponent },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { LoginComponent } from './pages/login/login.component';
+import { ErrorNotFoundComponent } from './pages/error-not-found/error-not-found.component';
 
 import { appRoutes } from './app-routes';
 import { environment } from 'src/environments/environment';
@@ -17,7 +18,8 @@ import { environment } from 'src/environments/environment';
     AppComponent,
     HomeComponent,
     AboutComponent,
-    LoginComponent
+    LoginComponent,
+    ErrorNotFoundComponent
   ],
   imports: [
     BrowserModule,
