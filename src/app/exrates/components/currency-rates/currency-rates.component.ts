30bcafe5f95f5229ed03ae609cf8cbe8e4fff70d
Michael Alt <mail@lean-stack.de>
2020-10-27 21:00:08 +0100

    Create the two views of the exrates module.

---
+++ src/app/exrates/components/currency-rates/currency-rates.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ex-currency-rates',
+  templateUrl: './currency-rates.component.html',
+  styleUrls: ['./currency-rates.component.css']
+})
+export class CurrencyRatesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/exrates/exrates.module.ts
+++ src/app/exrates/exrates.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ExratesRoutingModule } from './exrates-routing.module';
+import { CurrencyListComponent } from './components/currency-list/currency-list.component';
+import { CurrencyRatesComponent } from './components/currency-rates/currency-rates.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CurrencyListComponent, CurrencyRatesComponent],
   imports: [
     CommonModule,
     ExratesRoutingModule
