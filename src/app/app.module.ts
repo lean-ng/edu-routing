9e9ddaf9b15809812e5ab38afbb70f58df935458
Michael Alt <mail@lean-stack.de>
2020-10-27 20:08:13 +0100

    Define a wildcard route.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './core/components/home/home.component';
 import { AboutComponent } from './core/components/about/about.component';
+import { NotFoundComponent } from './core/components/not-found/not-found.component';
 
 const appRoutes: Route[] = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
