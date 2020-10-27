a080905682ae290bdaf15cf83bea3c3ff6f290bd
Michael Alt <mail@lean-stack.de>
2020-10-27 20:01:03 +0100

    Create two views.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
 import { ClarityModule } from '@clr/angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { AppComponent } from './app.component';
+import { HomeComponent } from './core/components/home/home.component';
+import { AboutComponent } from './core/components/about/about.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
