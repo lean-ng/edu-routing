43bf294949826a34a825c9ab6f36157fbfbc3309
Michael Alt <mail@lean-stack.de>
2020-10-27 21:04:21 +0100

    Add the routes and an nav anchor.

--- src/app/exrates/exrates-routing.module.ts
+++ src/app/exrates/exrates-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CurrencyListComponent } from './components/currency-list/currency-list.component';
+import { CurrencyRatesComponent } from './components/currency-rates/currency-rates.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'exrates/currencies', component: CurrencyListComponent },
+  { path: 'exrates/rates', component: CurrencyRatesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
