9df5874b614190a615e8184967137447f20ee92f
Michael Alt <mail@lean-stack.de>
2020-10-28 07:45:47 +0100

    Add crisis-center feature with secondary sidenav and child routes.

--- src/app/crisis-center/crisis-center-routing.module.ts
+++ src/app/crisis-center/crisis-center-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CrisisCenterHomeComponent } from './components/crisis-center-home/crisis-center-home.component';
+import { CrisisCenterComponent } from './components/crisis-center/crisis-center.component';
+import { CrisisDetailComponent } from './components/crisis-detail/crisis-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'crisis-center',
+  component: CrisisCenterComponent,
+  children: [
+    {
+      path: ':slug',
+      component: CrisisDetailComponent
+    },
+    {
+      path: '',
+      component: CrisisCenterHomeComponent
+    }
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/crisis-center/crisis-center.module.ts
+++ src/app/crisis-center/crisis-center.module.ts
@@ -2,11 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CrisisCenterRoutingModule } from './crisis-center-routing.module';
+import { CrisisCenterComponent } from './components/crisis-center/crisis-center.component';
+import { CrisisListComponent } from './components/crisis-list/crisis-list.component';
+import { CrisisDetailComponent } from './components/crisis-detail/crisis-detail.component';
+import { CrisisCenterHomeComponent } from './components/crisis-center-home/crisis-center-home.component';
+import { ClarityModule } from '@clr/angular';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CrisisCenterComponent,
+    CrisisListComponent,
+    CrisisDetailComponent,
+    CrisisCenterHomeComponent
+  ],
   imports: [
     CommonModule,
+    ClarityModule,
     CrisisCenterRoutingModule
   ]
 })
