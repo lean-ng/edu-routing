f180b6c032243cb3dd9dabde94ea566508439d57
Michael Alt <mail@lean-stack.de>
2020-10-28 05:39:19 +0100

    Add login page and route.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,15 @@ import { HomeComponent } from './core/components/home/home.component';
 import { AboutComponent } from './core/components/about/about.component';
 import { NotFoundComponent } from './core/components/not-found/not-found.component';
 import { ExratesModule } from './exrates/exrates.module';
+import { LogOnComponent } from './core/components/log-on/log-on.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AboutComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    LogOnComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -3,11 +3,13 @@ import { Route, RouterModule } from '@angular/router';
 
 import { AboutComponent } from './core/components/about/about.component';
 import { HomeComponent } from './core/components/home/home.component';
+import { LogOnComponent } from './core/components/log-on/log-on.component';
 import { NotFoundComponent } from './core/components/not-found/not-found.component';
 
 const appRoutes: Route[] = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'login', component: LogOnComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: NotFoundComponent },
 ];
